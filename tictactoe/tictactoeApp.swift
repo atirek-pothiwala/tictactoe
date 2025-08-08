2467a1de3be05b4668d7c921916de898b7bfe9d9
Atirek Pothiwala <atirekpothiwala@gmail.com>
2025-08-09 01:31:30 +0530

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Atirek Pothiwala on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
